0d829bbf47233f656cb4d0e5a1b1bb585c1faffc
nimrat1000 <2220guriii@gmail.com>
2026-06-03 02:40:11 -0400

    Update Buffer API request method to POST

--- test_buffer.py
+++ test_buffer.py
@@ -3,14 +3,24 @@ import requests
 
 API_KEY = os.getenv("BUFFER_API_KEY")
 
+url = "https://api.buffer.com/graphql"
+
 headers = {
-    "Authorization": f"Bearer {API_KEY}"
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
 }
 
-response = requests.get(
-    "https://api.buffer.com/graphql",
-    headers=headers
-)
+query = {
+    "query": """
+    query {
+      me {
+        id
+      }
+    }
+    """
+}
+
+response = requests.post(url, headers=headers, json=query)
 
 print("STATUS:", response.status_code)
-print(response.text[:500])
+print(response.text)
